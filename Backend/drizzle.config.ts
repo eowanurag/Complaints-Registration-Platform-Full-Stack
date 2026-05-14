cb131ef7d1278b32ce5cba855216c4cf332c41a1
anurag <eowanurag@gmail.com>
2026-05-14 15:55:13 +0530

    feat: add drizzle configuration file for postgresql database schema management

--- Backend/drizzle.config.ts
+++ Backend/drizzle.config.ts
@@ -1,4 +1,8 @@
 import { defineConfig } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+console.log('Using URL:', process.env.SUPABASE_URL ? 'YES (Masked)' : 'NO');
 
 export default defineConfig({
   schema: './src/db/schema.ts',
